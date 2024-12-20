39fbc9332c1e084c73e7d89b930fc2fe2078b63f
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2024-12-20 13:10:59 -0500

    update

---
+++ ExpressJS/Teoria/introducction.js
@@ -0,0 +1,20 @@
+" DEFINICION EXPRESS "
+
+"Express es un marco de trabajo de Javascript potente pero flexible para crear servidores web y API . Se puede utilizar para todo, desde servidores de archivos estáticos simples hasta JSON API para servidores de producción completos"
+
+" PARA PODER INICIALIZAR UN SERVIDOR EXPRESS WINDOWS - PASOS "
+" Podemos guiarnos de: https://www.geeksforgeeks.org/introduction-to-express/"
+
+// PASO 1 - COMMAND
+
+" npm init  "
+
+// PASO 2 - INSTALL EXPRESS
+
+" npm install express --save "
+
+// PASO 3 - EJECUTAR 
+
+" Dentro de nuestra carpeta y el ejecutable app.js "
+
+// node ./app.js
