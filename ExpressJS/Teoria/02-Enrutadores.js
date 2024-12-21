7f0c2f38e0863aa1309dfe109071e031203ebcac
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2024-12-20 22:40:18 -0500

    update teoric basic

---
+++ ExpressJS/Teoria/02-Enrutadores.js
@@ -0,0 +1,5 @@
+" COMO INTRUDUCCION 2  : DEFINICION - ENRUTADORES" 
+
+"Los enrutadores son versiones en miniatura de las aplicaciones Express: ofrecen funciones para gestionar la coincidencia de rutas, solicitudes y envían respuestas, pero no inician un servidor separado ni escuchan en sus propios puertos. Los enrutadores utilizan todos los metodos: .get(), .put(), .post(), y .delete()"
+
+// EL USO DE ENTURATADORES, LO PODEMOS VER EN EJEMPLO DE SERVER02 - Laboratorios.
